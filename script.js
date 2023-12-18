ab59ba23c59b399d1fea38d9f1942a95cd45db8b
Dome1992 <forlanodomenico92@mail.com>
2023-12-18 10:38:42 +0100

    feat - selectContact

--- script.js
+++ script.js
@@ -114,28 +114,19 @@ createApp({
             ]
         },
         
-      ]
+      ],
+      selectContact: 0, // si parte da nessun contatto selezionato
     };
-  }
+  },
+
+methods: {
+    showChat(index) {
+        this.selectContact= index;
+    },
+}, 
+
 }).mount('#app');
 
 
 
 
-/*contacts: [
-    {
-        name: 'Michele',
-        avatar: './img/avatar_1.png',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                message: 'Hai portato a spasso il cane?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                message: 'Ricordati di stendere i panni',
-                status: 'sent'
-            },
-            {*/
